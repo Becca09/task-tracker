7403c0a673635cb042bb05972f119366b9c81c8e
becca09 <adeolaadefuye@gmail.com>
2022-07-23 03:47:17 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: " Feb 5th at 2:30pm",
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: "School Appointment",
+      day: " Feb 6th at 1:30pm",
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: "food shopping",
+      day: " Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = {id, ...task}
+
+    setTask ([...tasks, newTask])
+  } 
+
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id));
+  };
+  const toggleRrminder = (id) => {
+    setTask(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+
+  };
+  return (    
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!setShowAddTask) } /> 
+      {showAddTask && <AddTask  onAdd = {addTask}/>}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRrminder} />
+      ) : (
+        "No Task To Show"
+      )}
     </div>
   );
 }
